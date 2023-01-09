c22ed4268f0f32c832caeca6aaaba2566e8a4f03
Miaowang-keep <mw123a@qq.com>
2023-01-09 09:26:22 +0000

    更新TS识别.vue文件的声明

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
+/**
+* @description: 解决ts不识别.vue的问题 ，做一个ts的全局声明
+* @author: 'miaowang'
+* @date : 2023-01-09 16:42:34
+*/
+
 /// <reference types="vite/client" />
+
+declare module "*.vue" {
+    import { DefineComponent } from "vue"
+    const component: DefineComponent<{}, {}, any>
+    export default component
+}
